dcb72ac265dfcc5f93aefd436db48cd6e51dd1a3
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-20 17:28:42 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <h1 className="bg-cyan-900">Hello World!</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
