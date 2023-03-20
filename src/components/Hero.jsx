53cdd4a502c481b0e50e10ffd366f24f2e7e37cb
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-20 17:57:30 +0100

    Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,13 +15,13 @@ const Hero = () => {
           <p className="md:text-5xl sm:text-4xl text-xl font-bold">
             Fast, flexible financing for
           </p>
-          <Typed
+          {/* <Typed
             className="md:text-5xl sm:text-4xl text-xl font-bold py-4 md:pl:4 pl-2"
             strings={["BTB", "BTC", "SASS"]}
             typeSpeed={120}
             backSpeed={140}
             loop
-          />
+          /> */}
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">
           Monitor your data analytics to increase revenue for BTB, BTC & SASS
